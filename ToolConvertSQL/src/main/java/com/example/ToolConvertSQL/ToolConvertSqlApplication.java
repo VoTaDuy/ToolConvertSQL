16a793a7c0ad64e3a70b0d7c04288a5809b626ef
VoTaDuy <154273585+VoTaDuy@users.noreply.github.com>
2026-04-21 22:27:28 +0700

    first commit

---
+++ ToolConvertSQL/src/main/java/com/example/ToolConvertSQL/ToolConvertSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ToolConvertSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolConvertSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolConvertSqlApplication.class, args);
+	}
+
+}
---
+++ ToolConvertSQL/src/test/java/com/example/ToolConvertSQL/ToolConvertSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToolConvertSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolConvertSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
